c32e1b307838c4e42fcbaf62aefab273390fefe7
azural <azural@azurals-MacBook-Pro.local>
2014-08-16 12:37:53 +0100

    add missing files

---
+++ Music Generator/Music Generator/audio_constants.h
@@ -0,0 +1,60 @@
+//
+//  audio_constants.h
+//  Music Generator
+//
+//  Created by azural on 16/08/2014.
+//  Copyright (c) 2014 com.azural. All rights reserved.
+//
+
+#ifndef Music_Generator_audio_constants_h
+#define Music_Generator_audio_constants_h
+
+
+static float a_min  = 0.0;
+static float a_init = 10.0;
+static float a_max  = 20.0;
+
+static float b_min  = 0.0;
+static float b_init = 40.0;
+static float b_max  = 80.0;
+
+static float c_min  = -25.0;
+static float c_init = 0.0;
+static float c_max  = 25.0;
+
+static float freq_min  = 100.0;
+static float freq_init = 550.0;
+static float freq_max  = 1000.0;
+
+/*
+ 
+interesting:
+20
+80
+-25
+100
+
+*/
+ 
+/*
+ 
+static float a_min  = 0.0;
+static float a_init = 13.0;
+static float a_max  = 26.0;
+ 
+static float b_min  = 0.0;
+static float b_init = 40.0;
+static float b_max  = 80.0;
+
+static float c_min  = -25.0;
+static float c_init = 0.0;
+static float c_max  = 25.0;
+ 
+static float freq_min  = 100.0;
+static float freq_init = 550.0;
+static float freq_max  = 1000.0;
+ 
+ 
+*/
+
+#endif
